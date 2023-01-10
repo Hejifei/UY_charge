be6e528182d8c9dd3ac8c486709ac6bc4a1b2fdf
hejifei <jifei.he@goodwe.com>
2023-01-10 22:24:09 +0800

    信息展示 静态页面

---
+++ miniprogram/components/blank/blank.ts
@@ -0,0 +1,15 @@
+
+Component({
+    properties: {
+      height: {
+        type: Number,
+        value: undefined,
+      },
+    },
+    data: {
+    },
+    created() {
+    },
+    methods: {
+    }
+  })
---
+++ miniprogram/components/circle_progress/circle_progress.ts
@@ -0,0 +1,31 @@
+import {tabbar_data_list, tabbar_info_view_value} from '../../common/index'
+
+
+Component({
+    properties: {
+      status: {
+        type: String,
+        value: undefined,
+      },
+      minutes: {
+          type: Number,
+          value: undefined,
+      },
+      electric_current: {
+          type: Number,
+          value: undefined,
+      },
+      voltage: {
+          type: Number,
+          value: undefined,
+      },
+      progress_value: {
+          type: Number,
+          value: undefined,
+      }
+    },
+    data: {
+    },
+    methods: {
+    }
+  })
